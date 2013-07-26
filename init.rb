8551aed2410a90b52503347115e481933bb5f592
Ian Chan <ichan@csusm.edu>
2013-07-26 10:46:08 -0700

    Initial plugin setup

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_add_frameworks do
+  name 'Redmine Add Frameworks plugin'
+  author 'Ian Chan'
+  description 'Use this plugin to add external libraries and frameworks - i.e. Font Awesome'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end

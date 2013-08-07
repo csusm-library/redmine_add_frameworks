8c39dc8222357ccd638e11518cb1f151fdb4dc8c
Ian Chan <ichan@csusm.edu>
2013-08-07 16:27:07 -0700

    Working on overrides to top menu

--- lib/hooks/view_layouts_base_html_head_hook.rb
+++ lib/hooks/view_layouts_base_html_head_hook.rb
@@ -2,7 +2,6 @@ module RedmineAddFrameworks
   module Hooks
     class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-        stylesheet_link_tag('//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css')
       end
     end
   end
